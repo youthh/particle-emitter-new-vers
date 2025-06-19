d7a261a526f14630a31e58c27ef2272603be5472
Valiavskiy-Lesha <Oleksii.Valiavskyi@onseo.biz>
2025-06-19 17:18:52 +0300

    some fix

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
   productionSourceMap: false,
-  publicPath: isProd
-    ? '/pparticles-editor/'
-    : '/',
+  publicPath: './',
+
 };
